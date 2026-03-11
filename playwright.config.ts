ac1d83b9a1819a03787bccdb5136e572a7f387b2
Anna Amidani <an5tash@gmail.com>
2026-03-11 14:30:56 +0000

    chore: configure playwright

---
+++ playwright.config.ts
@@ -0,0 +1,20 @@
+// playwright.config.ts
+import { defineConfig, devices } from '@playwright/test';
+
+const isCI = process.env.GITHUB_ACTIONS === 'true';
+const basePath = isCI ? '/nopo' : '';
+
+export default defineConfig({
+  testDir: './tests/e2e',
+  use: {
+    baseURL: `http://localhost:4321${basePath}`,
+  },
+  webServer: {
+    command: 'npm run preview',
+    url: `http://localhost:4321${basePath}/en/`,
+    reuseExistingServer: !isCI,
+  },
+  projects: [
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+  ],
+});
